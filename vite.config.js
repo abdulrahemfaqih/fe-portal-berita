70af4921e147a35748bf0c225d36d8a41ad42ee6
abdulrahemfaqih <faqih3935@gmail.com>
2025-01-13 01:52:18 +0700

    fix: laading state

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	server: {
-		proxy: {
-			"/api": {
-				target: "https://portal-berita-24c0bb377e97.herokuapp.com/",
-				changeOrigin: true,
-				headers: {
-					Accept: "application/json",
-					"Content-type": "application/json"
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '')
+
+	return {
+		plugins: [react()],
+		server: {
+			proxy: {
+				"/api": {
+					target: env.VITE_API_URL,
+					changeOrigin: true,
+					headers: {
+						Accept: "application/json",
+						"Content-type": "application/json"
+					}
 				}
 			}
 		}
